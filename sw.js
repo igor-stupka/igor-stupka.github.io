1e43eeea6fcd147c4e60f5641524fb80be56bee4
i.stupka <i.stupka@1plus1.corp>
2019-07-19 16:49:40 +0300

    sw fix

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 const CACHE_NAME = 'AST-3';
 const URLS_CACHE_ONLY = [
     "/fonts/montserrat-v13-cyrillic_latin-500.woff2",
-    "/fonts/montserrat-v13-cyrillic_latin-900.woff2",
+    "/fonts/montserrat-v13-cyrillic_latin-regular.woff2",
     "/img/icons/favicon.jpg",
     "/img/icons/webclip.jpg"
 ];
